2d2daaecc00242a22334fde238cfee2ffb47e580
Samjenglish <samenglish@cmail.carleton.ca>
2020-02-05 13:00:31 -0500

    Tests

--- src/main/java/com/example/labdemo/WebController.java
+++ src/main/java/com/example/labdemo/WebController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
@@ -11,5 +12,9 @@ public class WebController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="Sam") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+    @GetMapping("/test")
+    public @ResponseBody String testRoute() {
+        return "test";
     }
 }
