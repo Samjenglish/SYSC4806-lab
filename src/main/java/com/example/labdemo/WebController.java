ecd016dd8383b45bdee1f13c01fb08622a3cbd91
Samjenglish <Donglover69>
2020-02-05 16:40:13 -0500

    minor fixes

--- src/main/java/com/example/labdemo/WebController.java
+++ src/main/java/com/example/labdemo/WebController.java
@@ -18,40 +18,9 @@ public class WebController {
     }
 
     @Autowired
-    AddressBookRepository addressBookRepository;
+    private AddressBookRepository addressBookRepository;
     @Autowired
-    BuddyInfoRepository buddyInfoRepository;
-
-    private void populateRepositoriesOnStartUp(){
-        AddressBook book1 = new AddressBook("test book 1");
-        AddressBook book2 = new AddressBook("test book 2");
-        BuddyInfo bud1 = new BuddyInfo("Sam", "12");
-        BuddyInfo bud2 = new BuddyInfo("Austin", "4312");
-        BuddyInfo bud3 = new BuddyInfo("Nikola", "43");
-        BuddyInfo bud4 = new BuddyInfo("Hailey", "23");
-        BuddyInfo bud5 = new BuddyInfo("Diego", "8675");
-        BuddyInfo bud6 = new BuddyInfo("Greg", "987978");
-        book1.addBuddy(bud1);
-        book1.addBuddy(bud2);
-        book1.addBuddy(bud3);
-        bud1.setAddressBook(book1);
-        bud2.setAddressBook(book1);
-        bud3.setAddressBook(book1);
-        book2.addBuddy(bud4);
-        book2.addBuddy(bud5);
-        book2.addBuddy(bud6);
-        bud4.setAddressBook(book2);
-        bud5.setAddressBook(book2);
-        bud6.setAddressBook(book2);
-        addressBookRepository.save(book1);
-        addressBookRepository.save(book2);
-    }
-
-    WebController(AddressBookRepository repository1, BuddyInfoRepository repository2) {
-        this.addressBookRepository = repository1;
-        this.buddyInfoRepository = repository2;
-        populateRepositoriesOnStartUp();
-    }
+    private BuddyInfoRepository buddyInfoRepository;
 
     // BuddyInfo
     @ResponseBody
@@ -77,4 +46,29 @@ public class WebController {
     public @ResponseBody String testRoute() {
         return "test";
     }
+
+//    private void populateRepositoriesOnStartUp(){
+//        AddressBook book1 = new AddressBook("test book 1");
+//        AddressBook book2 = new AddressBook("test book 2");
+//        BuddyInfo bud1 = new BuddyInfo("Sam", "12");
+//        BuddyInfo bud2 = new BuddyInfo("Austin", "4312");
+//        BuddyInfo bud3 = new BuddyInfo("Nikola", "43");
+//        BuddyInfo bud4 = new BuddyInfo("Hailey", "23");
+//        BuddyInfo bud5 = new BuddyInfo("Diego", "8675");
+//        BuddyInfo bud6 = new BuddyInfo("Greg", "987978");
+//        book1.addBuddy(bud1);
+//        book1.addBuddy(bud2);
+//        book1.addBuddy(bud3);
+//        bud1.setAddressBook(book1);
+//        bud2.setAddressBook(book1);
+//        bud3.setAddressBook(book1);
+//        book2.addBuddy(bud4);
+//        book2.addBuddy(bud5);
+//        book2.addBuddy(bud6);
+//        bud4.setAddressBook(book2);
+//        bud5.setAddressBook(book2);
+//        bud6.setAddressBook(book2);
+//        addressBookRepository.save(book1);
+//        addressBookRepository.save(book2);
+//    }
 }
--- src/test/java/com/example/labdemo/WebLayerTest.java
+++ src/test/java/com/example/labdemo/WebLayerTest.java
@@ -8,11 +8,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WebLayerTest {
 
     @Autowired
@@ -20,7 +23,7 @@ public class WebLayerTest {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/test")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/test")).andExpect(status().isOk())
                 .andExpect(content().string(containsString("test")));
     }
 }
